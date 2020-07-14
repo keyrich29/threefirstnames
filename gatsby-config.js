2bb881170dfe60808e8c93c9cdcd65775a730440
Keith R <60330055+keyrich29@users.noreply.github.com>
2020-07-14 18:58:22 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Keith Rich`,
+      summary: `who lives and works in the Philly suburbs.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A starter blog for random musings.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `keith_rich`,
     },
   },
   plugins: [
